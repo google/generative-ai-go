2aea2a1706e3022f89b36b4f765037e4494cd393
bennhuang <bennhuang@tencent.com>
2024-09-19 16:34:47 +0800

    Update SendMessageStream to return errors early

--- genai/example_test.go
+++ genai/example_test.go
@@ -985,7 +985,10 @@ func ExampleChatSession_streaming() {
 		},
 	}
 
-	iter := cs.SendMessageStream(ctx, genai.Text("How many paws are in my house?"))
+	iter, err := cs.SendMessageStream(ctx, genai.Text("How many paws are in my house?"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	for {
 		resp, err := iter.Next()
 		if err == iterator.Done {
@@ -1017,9 +1020,12 @@ func ExampleChatSession_streamingWithImage() {
 		log.Fatal(err)
 	}
 
-	iter := cs.SendMessageStream(ctx,
+	iter, err := cs.SendMessageStream(ctx,
 		genai.Text("What family of instruments does this instrument belong to?"),
 		genai.ImageData("jpeg", imgData))
+	if err != nil {
+		log.Fatal(err)
+	}
 	for {
 		resp, err := iter.Next()
 		if err == iterator.Done {
