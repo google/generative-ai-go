93947f2fa80242956f9e54a324bd3e0fcec17509
Jonathan Amsterdam <jba@google.com>
2024-10-08 12:57:35 -0400

    all: fix license headers and add a check
    Replace all incorrect license headers with the correct one.
    
    Add a test that checks all Go files and shell scripts.
    
    Fixes #217.

--- devtools/generate_discovery_client.sh
+++ devtools/generate_discovery_client.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -e
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # This script generates the "discovery" client for the GenerativeLanguage API.
 # It is needed for file upload, which GAPIC clients don't support.
@@ -30,4 +43,8 @@ google-api-go-generator -api_json_file $docfile \
   -gendir internal \
   -internal_pkg github.com/google/generative-ai-go/genai/internal \
   -gensupport_pkg github.com/google/generative-ai-go/genai/internal/gensupport
+
+# Replace license with the proper one for this repo.
+file=internal/generativelanguage/v1beta/generativelanguage-gen.go
+cat license.txt <(tail +5 $file) | sponge $file
 
