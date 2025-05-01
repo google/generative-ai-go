51f859c474cbabf87eab2dcb92d524fdc85315b6
Mark Daoust <markdaoust@google.com>
2025-05-01 11:33:03 -0700

    Update doc.go from README

--- genai/doc.go
+++ genai/doc.go
@@ -12,11 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package genai is a client for the Google AI generative models.
+// # [Deprecated] Google AI Go SDK for the Gemini API
 //
-// NOTE: This client uses the v1beta version of the API.
+// With Gemini 2.0, we took the chance to create a single unified SDK for all developers who want to use Google's GenAI models (Gemini, Veo, Imagen, etc). As part of that process, we took all of the feedback from this SDK and what developers like about other SDKs in the ecosystem to create the [Google Gen AI SDK](https://github.com/googleapis/go-genai). 
+//
+// The Gemini API docs are fully updated to show examples of the new Google Gen AI SDK: [Get started](https://ai.google.dev/gemini-api/docs/quickstart?lang=go).
+//
+// We know how disruptive an SDK change can be and don't take this change lightly, but our goal is to create an extremely simple and clear path for developers to build with our models so it felt necessary to make this change.
+//
+// Thank you for building with Gemini and [let us know](https://discuss.ai.google.dev/c/gemini-api/4) if you need any help!
+//
+// **Please be advised that this repository is now considered legacy.** For the latest features, performance improvements, and active development, we strongly recommend migrating to the official **[Google Generative AI SDK for Go](https://github.com/googleapis/go-genai)**.
+//
+// **Support Plan for this Repository:**
+//
+// *   **Limited Maintenance:** Development is now restricted to **critical bug fixes only**. No new features will be added.
+// *   **Purpose:** This limited support aims to provide stability for users while they transition to the new SDK.
+// *   **End-of-Life Date:** All support for this repository (including bug fixes) will permanently end on **August 31st, 2025**.
+//
+// We encourage all users to begin planning their migration to the [Google Generative AI SDK](https://github.com/googleapis/go-genai) to ensure continued access to the latest capabilities and support.
+//
 //
 // # Getting started
+//
+// NOTE: This client uses the v1beta version of the API.
 //
 // Reading the [examples] is the best way to learn how to use this package.
 //
